53c42d321062428c6e64537c5cc94ca80a5bd7c4
ivanogor <ivanogor23@gmail.com>
2024-03-04 16:42:25 +0800

    add toString

--- src/main/java/com/skypro/cart/model/Item.java
+++ src/main/java/com/skypro/cart/model/Item.java
@@ -1,5 +1,7 @@
 package com.skypro.cart.model;
 
+import java.util.Objects;
+
 public class Item {
     private String name;
     private int price;
@@ -7,5 +9,26 @@ public class Item {
     public Item(String name, int price) {
         this.name = name;
         this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return price == item.price && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
